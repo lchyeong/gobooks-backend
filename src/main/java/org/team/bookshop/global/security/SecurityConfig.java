72fbb29ba5f771d1fcd7b3ecfb9211583003aeb7
[Cloud3] 오재영 <wodud.oh@gmail.com>
2024-06-05 21:21:32 +0900

    chore:리엑트 서버에서 POST 접근을 위한 시큐리티에 Credential true 세팅 추가

--- src/main/java/org/team/bookshop/global/security/SecurityConfig.java
+++ src/main/java/org/team/bookshop/global/security/SecurityConfig.java
@@ -15,7 +15,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.team.bookshop.domain.user.entity.UserRole;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import org.springframework.web.filter.CorsFilter;
 import org.team.bookshop.domain.user.service.UserService;
 import org.team.bookshop.global.error.exception.SecurityConfigurationException;
 
@@ -51,6 +55,30 @@ public class SecurityConfig {
 		}catch (Exception e) {
 			throw new SecurityConfigurationException("Security configuration failed");
 		}
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowCredentials(true);
+
+		configuration.addAllowedOrigin("http://localhost:[*]");
+		configuration.addAllowedHeader("*");
+
+		configuration.addAllowedMethod("GET");
+		configuration.addAllowedMethod("POST");
+		configuration.addAllowedMethod("PUT");
+		configuration.addAllowedMethod("DELETE");
+		configuration.addAllowedMethod("FETCH");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		return new CorsFilter(corsConfigurationSource());
 	}
 
 	@Bean
