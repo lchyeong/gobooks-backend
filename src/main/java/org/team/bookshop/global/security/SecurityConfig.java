acc3e015c7278b46885cd9caeb0a2f81e2216011
[Cloud3] 이찬형 <tolnhappy5@naver.com>
2024-06-07 12:47:44 +0900

    feat: OAuth2-Naver 기능구현
    user provideId 필드/oauth2.yml/customAu..Handler/OAuth2 의존성 추가

--- src/main/java/org/team/bookshop/global/security/SecurityConfig.java
+++ src/main/java/org/team/bookshop/global/security/SecurityConfig.java
@@ -16,10 +16,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
-
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.team.bookshop.domain.user.repository.UserRepository;
 import org.team.bookshop.domain.user.service.UserService;
 import org.team.bookshop.global.error.exception.SecurityConfigurationException;
 
@@ -28,76 +28,83 @@ import org.team.bookshop.global.error.exception.SecurityConfigurationException;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-	private UserService userService;
-	private JwtTokenizer jwtTokenizer;
+    private UserService userService;
+    private final JwtTokenizer jwtTokenizer;
+    private final UserRepository userRepository;
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws SecurityConfigurationException {
-		try{
-			http
-					.csrf(AbstractHttpConfigurer::disable)
-					.sessionManagement(session -> session
-							.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-					)
-					.authorizeHttpRequests((authorizeRequests) ->
-							authorizeRequests
-									.requestMatchers(HttpMethod.GET, "/").permitAll()
-									.requestMatchers(HttpMethod.POST, "/api/auth/").permitAll()
-									.requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+        throws SecurityConfigurationException {
+        try {
+            http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                            .requestMatchers(HttpMethod.GET, "/").permitAll()
+                            .requestMatchers(HttpMethod.POST, "/api/auth/").permitAll()
+                            .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
 //									.requestMatchers("/api/admin/**").hasAnyAuthority(UserRole.ADMIN.getRole())
 //									.requestMatchers("/api/users/**").hasAnyAuthority(UserRole.ADMIN.getRole(), UserRole.USER.getRole())
-									.anyRequest().permitAll() // jwt 완성 전까지는 다 접근 가능하게 임시로 세팅
-					)
-					.addFilterBefore(new JwtCustomFilter(userService, jwtTokenizer), UsernamePasswordAuthenticationFilter.class)
-					.formLogin(AbstractHttpConfigurer::disable)
-					.httpBasic(AbstractHttpConfigurer::disable);
-			return http.build();
-		}catch (Exception e) {
-			throw new SecurityConfigurationException("Security configuration failed");
-		}
-	}
+                            .anyRequest().permitAll() // jwt 완성 전까지는 다 접근 가능하게 임시로 세팅
+                )
+                .oauth2Login(oauth2Login ->
+                    oauth2Login
+                        .successHandler(new CustomAuthSuccessHandler(jwtTokenizer, userRepository))
+                )
+                .addFilterBefore(new JwtCustomFilter(userService, jwtTokenizer),
+                    UsernamePasswordAuthenticationFilter.class)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+            return http.build();
+        } catch (Exception e) {
+            throw new SecurityConfigurationException("Security configuration failed");
+        }
+    }
 
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowCredentials(true);
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
 
-		configuration.addAllowedOrigin("http://localhost:3000");
-		configuration.addAllowedHeader("*");
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedHeader("*");
 
-		configuration.addAllowedMethod("GET");
-		configuration.addAllowedMethod("POST");
-		configuration.addAllowedMethod("PUT");
-		configuration.addAllowedMethod("DELETE");
-		configuration.addAllowedMethod("FETCH");
+        configuration.addAllowedMethod("GET");
+        configuration.addAllowedMethod("POST");
+        configuration.addAllowedMethod("PUT");
+        configuration.addAllowedMethod("DELETE");
+        configuration.addAllowedMethod("FETCH");
 
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
-	@Bean
-	public CorsFilter corsFilter() {
-		return new CorsFilter(corsConfigurationSource());
-	}
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	@Bean
-	@ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
-	public WebSecurityCustomizer h2ConsoleCustomizer() {
-		return web -> web.ignoring()
-				.requestMatchers(PathRequest.toH2Console());
-	}
+    @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
+    public WebSecurityCustomizer h2ConsoleCustomizer() {
+        return web -> web.ignoring()
+            .requestMatchers(PathRequest.toH2Console());
+    }
 
-	@Bean
-	@ConditionalOnProperty(name = "springdoc.swagger-ui.enabled", havingValue = "true")
-	public WebSecurityCustomizer swaggerCustomizer() {
-		return web -> web.ignoring()
-				.requestMatchers("/swagger-ui/**", "/v3/api-docs/**");
-	}
+    @Bean
+    @ConditionalOnProperty(name = "springdoc.swagger-ui.enabled", havingValue = "true")
+    public WebSecurityCustomizer swaggerCustomizer() {
+        return web -> web.ignoring()
+            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**");
+    }
 
 }
