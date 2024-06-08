e7df745bf903b88b542c19d9df2a8c8571997b10
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-06-08 16:54:58 +0900

    Fix: product 페이징 조회 쿼리문 수정

--- src/main/java/org/team/bookshop/domain/product/repository/ProductRepository.java
+++ src/main/java/org/team/bookshop/domain/product/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package org.team.bookshop.domain.product.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.team.bookshop.domain.product.entity.Product;
 
@@ -11,5 +13,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
   Page<Product> findAll(Pageable pageable); // 모든 상품 페이징 조회
 
-  Page<Product> findByCategory(Long categoryId, Pageable pageable); // 카테고리별 상품 페이징 조회
+  @Query("SELECT p FROM Product p JOIN p.bookCategories bc WHERE bc.category.id = :categoryId")
+  Page<Product> findByCategory(@Param("categoryId") Long categoryId,
+      Pageable pageable); // 카테고리별 상품 페이징 조회
 }
