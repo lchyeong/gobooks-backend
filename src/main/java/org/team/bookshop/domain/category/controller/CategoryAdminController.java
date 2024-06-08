619008d3c2ba9ae99312030d7fe8450b2a52dea2
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-06-08 14:52:44 +0900

    feat: CategoryController - admin/view로 분리, CategoryService 메소드 수정

--- src/main/java/org/team/bookshop/domain/category/controller/CategoryAdminController.java
+++ src/main/java/org/team/bookshop/domain/category/controller/CategoryAdminController.java
@@ -1,10 +1,8 @@
 package org.team.bookshop.domain.category.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,53 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 import org.team.bookshop.domain.category.dto.CategoryCreateRequestDto;
 import org.team.bookshop.domain.category.dto.CategoryResponseDto;
 import org.team.bookshop.domain.category.dto.CategoryUpdateRequestDto;
-import org.team.bookshop.domain.category.entity.Category;
-import org.team.bookshop.domain.category.repository.CategoryRepository;
 import org.team.bookshop.domain.category.service.CategoryService;
-import org.team.bookshop.global.error.ErrorCode;
-import org.team.bookshop.global.error.exception.ApiException;
 
 @RestController
 @RequestMapping("/api")
-public class CategoryController {
+public class CategoryAdminController {
 
-  @Autowired
   private final CategoryService categoryService;
-  @Autowired
-  private CategoryRepository categoryRepository;
 
-  public CategoryController(CategoryService categoryService) {
+  public CategoryAdminController(CategoryService categoryService) {
     this.categoryService = categoryService;
   }
-
-  // 전체 사용자 ========
-
-  // READ
-  @GetMapping("/categories/{categoryId}")
-  public ResponseEntity<CategoryResponseDto> getCategory(
-      @PathVariable Long categoryId) {
-    CategoryResponseDto categoryResponseDto = categoryService.getCategoryWithChildren(
-        categoryId);
-    return ResponseEntity.ok(categoryResponseDto);
-  }
-
-  // 관리자 ============
 
   // CREATE
   @PostMapping("/admin/categories")
   public ResponseEntity<CategoryResponseDto> createCategory(
       @RequestBody CategoryCreateRequestDto categoryCreateRequestDto) {
-    Long parentId = categoryCreateRequestDto.getParentId();
-    Category parent = null;
-    if (parentId != null) {
-      parent = categoryRepository.findById(parentId)
-          .orElseThrow(() -> new ApiException(ErrorCode.ENTITY_NOT_FOUND));
-    }
-    Category category = new Category();
-    category.setName(categoryCreateRequestDto.getName());
-    category.setParent(parent);
-    Category savedCategory = categoryRepository.save(category);
-    CategoryResponseDto responseDto = CategoryResponseDto.fromEntity(savedCategory);
+    CategoryResponseDto responseDto = categoryService.createCategory(categoryCreateRequestDto);
     return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
   }
 
