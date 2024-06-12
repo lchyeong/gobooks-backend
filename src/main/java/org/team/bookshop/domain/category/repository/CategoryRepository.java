951b9f30674d3a8f04bd3b258f30644f52705fea
[Cloud3] 이찬형 <tolnhappy5@naver.com>
2024-06-12 23:14:26 +0900

    refactor: 카테고리 쿼리 수정

--- src/main/java/org/team/bookshop/domain/category/repository/CategoryRepository.java
+++ src/main/java/org/team/bookshop/domain/category/repository/CategoryRepository.java
@@ -17,22 +17,29 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 //        "WHERE c.id = :categoryId")
 //    List<Product> findByCategoryId(@Param("categoryId") Long categoryId);
 
-  @Query("SELECT c FROM Category c "
-      + "LEFT JOIN FETCH c.children "
-      + "WHERE c.id = :parentId")
-  Optional<Category> findByIdWithChildren(@Param("parentId") Long parentId);
+    @Query("SELECT c FROM Category c "
+        + "LEFT JOIN FETCH c.children "
+        + "WHERE c.id = :parentId")
+    Optional<Category> findByIdWithChildren(@Param("parentId") Long parentId);
 
-  @Query("SELECT c FROM Category c "
-      + "WHERE c.parent IS NULL")
-  List<Category> findRootCategory();
+    @Query(value = "WITH RECURSIVE Category_Hierarchy(id, name, parent_id, level) AS (" +
+        "SELECT id, name, parent_id, 1 AS level " +
+        "FROM Category " +
+        "WHERE parent_id IS NULL " +
+        "UNION ALL " +
+        "SELECT c.id, c.name, c.parent_id, ch.level + 1 " +
+        "FROM Category c " +
+        "JOIN Category_Hierarchy ch ON c.parent_id = ch.id) " +
+        "SELECT * FROM Category_Hierarchy", nativeQuery = true)
+    List<Object[]> findRootCategory();
 
 //    @Query("SELECT c FROM Category c "
 //        + "WHERE c.parent.id = :parentId")
 //    List<Category> findByParentId(@Param("parentId") Long parentId);
 
-  @Query("SELECT c FROM Category c "
-      + "WHERE c.id = :categoryId "
-      + "OR c.parent.id = :categoryId "
-      + "OR c.parent.parent.id = :categoryId")
-  List<Category> findAllSubcategories(@Param("categoryId") Long categoryId);
+    @Query("SELECT c FROM Category c "
+        + "WHERE c.id = :categoryId "
+        + "OR c.parent.id = :categoryId "
+        + "OR c.parent.parent.id = :categoryId")
+    List<Category> findAllSubcategories(@Param("categoryId") Long categoryId);
 }
