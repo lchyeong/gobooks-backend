ee05ff07dd0436b9da1fd0758244465cff0e55a8
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-06-14 01:31:32 +0900

    Feat: 카테고리 Breadcrumb 구현 api 추가

--- src/main/java/org/team/bookshop/domain/category/controller/CategoryViewController.java
+++ src/main/java/org/team/bookshop/domain/category/controller/CategoryViewController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.team.bookshop.domain.category.dto.CategoryChildrenResponseDto;
+import org.team.bookshop.domain.category.dto.CategoryDto;
 import org.team.bookshop.domain.category.dto.CategoryResponseDto;
 import org.team.bookshop.domain.category.service.CategoryService;
 
@@ -14,39 +15,46 @@ import org.team.bookshop.domain.category.service.CategoryService;
 @RequestMapping("/api")
 public class CategoryViewController {
 
-    private final CategoryService categoryService;
+  private final CategoryService categoryService;
 
-    public CategoryViewController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
+  public CategoryViewController(CategoryService categoryService) {
+    this.categoryService = categoryService;
+  }
 
-    @GetMapping("/categories")
-    public ResponseEntity<List<CategoryResponseDto>> getRootCategories() {
-        List<CategoryResponseDto> rootCategories = categoryService.getRootCategories();
-        return ResponseEntity.ok(rootCategories);
-    }
+  @GetMapping("/categories")
+  public ResponseEntity<List<CategoryResponseDto>> getRootCategories() {
+    List<CategoryResponseDto> rootCategories = categoryService.getRootCategories();
+    return ResponseEntity.ok(rootCategories);
+  }
 
-    @GetMapping("/categories/hierarchy")
-    public ResponseEntity<List<CategoryResponseDto>> getCategoryHierarchy() {
-        List<CategoryResponseDto> categoryHierarchy = categoryService.getCategoryHierarchy();
-        return ResponseEntity.ok(categoryHierarchy);
-    }
+  @GetMapping("/categories/hierarchy")
+  public ResponseEntity<List<CategoryResponseDto>> getCategoryHierarchy() {
+    List<CategoryResponseDto> categoryHierarchy = categoryService.getCategoryHierarchy();
+    return ResponseEntity.ok(categoryHierarchy);
+  }
 
-    // READ
-    // 하위 계층을 모두 조회
-    @GetMapping("/categories/{categoryId}")
-    public ResponseEntity<CategoryResponseDto> getCategoryWithChildren(
-        @PathVariable Long categoryId) {
-        CategoryResponseDto responseDto = categoryService.getCategoryWithChildren(categoryId);
-        return ResponseEntity.ok(responseDto);
-    }
+  // READ
+  // 하위 계층을 모두 조회
+  @GetMapping("/categories/{categoryId}")
+  public ResponseEntity<CategoryResponseDto> getCategoryWithChildren(
+      @PathVariable Long categoryId) {
+    CategoryResponseDto responseDto = categoryService.getCategoryWithChildren(categoryId);
+    return ResponseEntity.ok(responseDto);
+  }
 
-    // 바로 아래 depth만 조회
-    @GetMapping("/categories/{categoryId}/children")
-    public ResponseEntity<List<CategoryChildrenResponseDto>> getCategoryWithDirectChildren(
-        @PathVariable Long categoryId) {
-        List<CategoryChildrenResponseDto> children = categoryService.getCategoryWithDirectChildren(
-            categoryId);
-        return ResponseEntity.ok(children);
-    }
+  // 바로 아래 depth만 조회
+  @GetMapping("/categories/{categoryId}/children")
+  public ResponseEntity<List<CategoryChildrenResponseDto>> getCategoryWithDirectChildren(
+      @PathVariable Long categoryId) {
+    List<CategoryChildrenResponseDto> children = categoryService.getCategoryWithDirectChildren(
+        categoryId);
+    return ResponseEntity.ok(children);
+  }
+
+  // BreadCrumb
+  @GetMapping("/categories/{categoryId}/breadcrumbs")
+  public ResponseEntity<List<CategoryDto>> getBreadcrumbs(@PathVariable Long categoryId) {
+    List<CategoryDto> breadcrumbs = categoryService.getBreadcrumbs(categoryId);
+    return ResponseEntity.ok(breadcrumbs);
+  }
 }
