42c1cb7dc880868a2c447cea1acac5448e8edae2
[Cloud3] 오재영 <wodud.oh@gmail.com>
2024-06-13 11:35:52 +0900

    chore: adminController 클래스명 변경 및 userController에서 admin의 접근이 필요한 메서드 이동함.

---
+++ src/main/java/org/team/bookshop/domain/user/controller/UserAdminController.java
@@ -0,0 +1,25 @@
+package org.team.bookshop.domain.user.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.team.bookshop.domain.user.dto.UserPostDto;
+import org.team.bookshop.domain.user.service.UserService;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+public class UserAdminController {
+
+  private final UserService userService;
+
+  @GetMapping
+  public ResponseEntity<List<UserPostDto>> getAllUsers() {
+    return ResponseEntity.ok(userService.getAllUsers());
+  }
+}
