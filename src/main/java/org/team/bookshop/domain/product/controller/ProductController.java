b843e9c99e417f93629a7ef9d4425c55f039a5b3
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-06-13 15:55:32 +0900

    refactor: Product 페이징 조회 queryDSL로 구현

--- src/main/java/org/team/bookshop/domain/product/controller/ProductController.java
+++ src/main/java/org/team/bookshop/domain/product/controller/ProductController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.team.bookshop.domain.product.dto.AddProductRequest;
+import org.team.bookshop.domain.product.dto.ProductDto;
 import org.team.bookshop.domain.product.dto.ProductResponse;
 import org.team.bookshop.domain.product.dto.UpdateProductRequest;
 import org.team.bookshop.domain.product.entity.Product;
@@ -26,60 +27,69 @@ import org.team.bookshop.domain.product.service.ProductService;
 @RequestMapping("/api/products")
 public class ProductController {
 
-    private final ProductService productService;
+  private final ProductService productService;
 
-    //상품 추가(관리자)
-    @PostMapping
-    public ResponseEntity<AddProductRequest> addProduct(
-        @RequestBody @Valid AddProductRequest request) {
-        AddProductRequest savedProduct = productService.createProduct(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
-    }
+  //상품 추가(관리자)
+  @PostMapping
+  public ResponseEntity<AddProductRequest> addProduct(
+      @RequestBody @Valid AddProductRequest request) {
+    AddProductRequest savedProduct = productService.createProduct(request);
+    return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
+  }
 
-    //모든 상품 조회(구매자)
-    @GetMapping
-    public ResponseEntity<List<ProductResponse>> findAllProducts() {
-        List<ProductResponse> products = productService.findAll();
-        return ResponseEntity.ok(products);
-    }
+  //모든 상품 조회(구매자)
+  @GetMapping
+  public ResponseEntity<List<ProductResponse>> findAllProducts() {
+    List<ProductResponse> products = productService.findAll();
+    return ResponseEntity.ok(products);
+  }
 
-    //controller 분리(관리자, 구매자)
-    //삼품 조회(구매자)
-    @GetMapping("/{id}")
-    public ResponseEntity<ProductResponse> findProduct(@PathVariable Long id) {
-        ProductResponse product = productService.findById(id);
-        return ResponseEntity.ok(product);
-    }
+  //controller 분리(관리자, 구매자)
+  //삼품 조회(구매자)
+  @GetMapping("/{id}")
+  public ResponseEntity<ProductResponse> findProduct(@PathVariable Long id) {
+    ProductResponse product = productService.findById(id);
+    return ResponseEntity.ok(product);
+  }
 
-    //상품 삭제(관리자)
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        productService.delete(id);
-        return ResponseEntity.ok().build();
-    }
+  //상품 삭제(관리자)
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+    productService.delete(id);
+    return ResponseEntity.ok().build();
+  }
 
-    //상품 수정(관리자)
-    @PutMapping("/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable long id,
-        @Valid @RequestBody UpdateProductRequest request) {
-        Product updatedProduct = productService.update(id, request);
-        return ResponseEntity.ok(updatedProduct);
-    }
+  //상품 수정(관리자)
+  @PutMapping("/{id}")
+  public ResponseEntity<Product> updateProduct(@PathVariable long id,
+      @Valid @RequestBody UpdateProductRequest request) {
+    Product updatedProduct = productService.update(id, request);
+    return ResponseEntity.ok(updatedProduct);
+  }
 
-    // 특정 카테고리 조회
-    @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<ProductResponse>> getProductsByCategory(
-        @PathVariable Long categoryId) {
-        List<ProductResponse> productResponses = productService.getProductsByCategoryId(categoryId);
-        return ResponseEntity.ok(productResponses);
-    }
+//  // 특정 카테고리 조회
+//  @GetMapping("/category/{categoryId}")
+//  public ResponseEntity<List<ProductResponse>> getProductsByCategory(
+//      @PathVariable Long categoryId) {
+//    List<ProductResponse> productResponses = productService.getProductsByCategoryId(categoryId);
+//    return ResponseEntity.ok(productResponses);
+//}
 
-    @GetMapping("/category/{categoryId}/paged")
-    public ResponseEntity<Page<ProductResponse>> getProductsByCategoryPaged(
-        @PathVariable Long categoryId, Pageable pageable) {
-        Page<ProductResponse> productResponses = productService.getProductsByCategoryId(categoryId,
-            pageable);
-        return ResponseEntity.ok(productResponses);
-    }
+//  @GetMapping("/category/{categoryId}/paged")
+//  public ResponseEntity<Page<ProductResponse>> getProductsByCategoryPaged(
+//      @PathVariable Long categoryId,
+//      @PageableDefault(sort = "created_at", direction = Sort.Direction.DESC) Pageable pageable
+//  ) {
+//    Page<ProductResponse> productResponses = productService.getProductsByCategoryId(categoryId,
+//        pageable);
+//    return ResponseEntity.ok(productResponses);
+//  }
+
+  // 특정 카테고리 조회 - paging (queryDSL 사용)
+  @GetMapping("/category/{categoryId}/paged")
+  public Page<ProductDto> getProductsByCategoryPaged(@PathVariable Long categoryId,
+      Pageable pageable) {
+    return productService.getProductsByCategoryId(categoryId, pageable);
+  }
 }
 
