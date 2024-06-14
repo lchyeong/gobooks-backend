9084d5c179ea5314545d349227ddc41dd130588e
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-06-14 17:47:04 +0900

    Refactor: category 컨트롤러명 변경, 불필요한 주석 삭제, product 컨트롤러 권한별 분리, product 서비스 메서드명 일관성있게 정리  / ProductService - createProduct 수정 필요함

--- src/main/java/org/team/bookshop/domain/category/controller/CategoryAdminController.java
+++ src/main/java/org/team/bookshop/domain/category/controller/CategoryAdminController.java
@@ -1,8 +1,8 @@
 package org.team.bookshop.domain.category.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,15 +16,12 @@ import org.team.bookshop.domain.category.dto.CategoryUpdateRequestDto;
 import org.team.bookshop.domain.category.service.CategoryService;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
 //@PreAuthorize("hasAnyRole('ROLE_ADMIN')") // 프론트 accsstoken 인증 문제 해결 전까지는 모든 요청 스킵함.
 public class CategoryAdminController {
 
   private final CategoryService categoryService;
-
-  public CategoryAdminController(CategoryService categoryService) {
-    this.categoryService = categoryService;
-  }
 
   // CREATE
   @PostMapping("/admin/categories")
