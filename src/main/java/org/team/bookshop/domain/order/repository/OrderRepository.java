bb3deb1cef9e49ea17f0eeda052e14c56ab4cbea
[Cloud3] 이찬형 <tolnhappy5@naver.com>
2024-06-16 00:23:41 +0900

    feat: 회원탈퇴관련 연관 레포지토리 수정

--- src/main/java/org/team/bookshop/domain/order/repository/OrderRepository.java
+++ src/main/java/org/team/bookshop/domain/order/repository/OrderRepository.java
@@ -8,19 +8,21 @@ import org.team.bookshop.domain.order.entity.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-  @Query("select o from Order o " +
-          "join fetch o.orderItems oi " +
-          "join fetch o.delivery d " +
-          "join fetch d.address " +
-          "join fetch oi.product " +
-          "where o.id = :orderId")
-  Order findWithAllRelatedEntityById(@Param("orderId") Long orderId);
+    @Query("select o from Order o " +
+        "join fetch o.orderItems oi " +
+        "join fetch o.delivery d " +
+        "join fetch d.address " +
+        "join fetch oi.product " +
+        "where o.id = :orderId")
+    Order findWithAllRelatedEntityById(@Param("orderId") Long orderId);
 
-  @Query("select o from Order o " +
-          "join fetch o.orderItems oi " +
-          "join fetch oi.product " +
-          "where o.id = :orderId")
-  Order findWithOrderItems(@Param("orderId") long orderId);
+    @Query("select o from Order o " +
+        "join fetch o.orderItems oi " +
+        "join fetch oi.product " +
+        "where o.id = :orderId")
+    Order findWithOrderItems(@Param("orderId") long orderId);
 
-  Optional<Order> findByMerchantId(String merchantId);
+    Optional<Order> findByMerchantId(String merchantId);
+
+    void deleteByDeliveryId(Long deliveryId);
 }
