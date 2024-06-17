756a19c4f1bca581bc7c68fc1cdea31bdd3fc639
[cloud3] 정동희 <xzxy6036@naver.com>
2024-06-17 16:30:03 +0900

    FEAT : delivery 저장기능 추가

--- src/main/java/org/team/bookshop/domain/order/controller/DeliveryController.java
+++ src/main/java/org/team/bookshop/domain/order/controller/DeliveryController.java
@@ -5,23 +5,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.team.bookshop.domain.order.Service.DeliveryService;
 import org.team.bookshop.domain.order.dto.CreateDeliveryRequest;
 import org.team.bookshop.domain.order.repository.DeliveryRepository;
 import org.team.bookshop.domain.order.repository.OrderRepository;
+import org.team.bookshop.domain.user.entity.User;
 import org.team.bookshop.domain.user.repository.AddressRepository;
+import org.team.bookshop.domain.user.repository.UserRepository;
+import org.team.bookshop.global.error.ErrorCode;
+import org.team.bookshop.global.error.exception.ApiException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/delivery")
 public class DeliveryController {
 
-  private final DeliveryRepository deliveryRepository;
-  private final OrderRepository orderRepository;
-  private final AddressRepository addressRepository;
+  private final DeliveryService deliveryService;
 
   @PostMapping("/create")
   public void createDelivery(@RequestBody CreateDeliveryRequest createDeliveryRequest) {
-    return;
+    deliveryService.createDelivery(createDeliveryRequest);
   }
 
 }
