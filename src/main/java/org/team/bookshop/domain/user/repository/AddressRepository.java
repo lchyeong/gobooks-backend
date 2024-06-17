7e6238f72f82296750e5421811eac73df2452317
[cloud3] 정동희 <xzxy6036@naver.com>
2024-06-17 15:54:36 +0900

    FEAT : Address 엔티티 index설정 및 Repository findByLabel추가

--- src/main/java/org/team/bookshop/domain/user/repository/AddressRepository.java
+++ src/main/java/org/team/bookshop/domain/user/repository/AddressRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.team.bookshop.domain.order.entity.Delivery;
 import org.team.bookshop.domain.user.entity.Address;
+import org.team.bookshop.domain.user.entity.User;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
@@ -15,5 +16,6 @@ public interface AddressRepository extends JpaRepository<Address, Long> {
 
     Address findByDelivery(Delivery delivery);
 
-    List<Address> findByLabel(String label);
+
+    List<Address> findByUserAndLabel(User user, String label);
 }
