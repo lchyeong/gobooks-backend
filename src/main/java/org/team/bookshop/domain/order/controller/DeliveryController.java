248e54a18166de759993e943ccd83952ec140bf2
[cloud3] 정동희 <xzxy6036@naver.com>
2024-06-18 15:23:39 +0900

    Feat : Delivery Controller 응답 api변경

--- src/main/java/org/team/bookshop/domain/order/controller/DeliveryController.java
+++ src/main/java/org/team/bookshop/domain/order/controller/DeliveryController.java
@@ -1,6 +1,7 @@
 package org.team.bookshop.domain.order.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.team.bookshop.domain.user.repository.AddressRepository;
 import org.team.bookshop.domain.user.repository.UserRepository;
 import org.team.bookshop.global.error.ErrorCode;
 import org.team.bookshop.global.error.exception.ApiException;
+import org.team.bookshop.global.response.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,8 +25,9 @@ public class DeliveryController {
   private final DeliveryService deliveryService;
 
   @PostMapping("/create")
-  public void createDelivery(@RequestBody CreateDeliveryRequest createDeliveryRequest) {
+  public ResponseEntity<? extends ApiResponse<?>> createDelivery(@RequestBody CreateDeliveryRequest createDeliveryRequest) {
     deliveryService.createDelivery(createDeliveryRequest);
+    return ResponseEntity.ok(ApiResponse.success());
   }
 
 }
