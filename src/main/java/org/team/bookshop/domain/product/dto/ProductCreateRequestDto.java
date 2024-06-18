637fe9b53e15edf7b58395c1d6c71cb0cb0deb81
[Cloud3] 이찬형 <tolnhappy5@naver.com>
2024-06-18 16:41:06 +0900

    feat: 상품관리 이미지 멀티파트 기능추가

--- src/main/java/org/team/bookshop/domain/product/dto/ProductCreateRequestDto.java
+++ src/main/java/org/team/bookshop/domain/product/dto/ProductCreateRequestDto.java
@@ -12,43 +12,41 @@ import org.team.bookshop.domain.product.entity.Product;
 @NoArgsConstructor
 public class ProductCreateRequestDto {
 
-  @NotBlank(message = "Title is required")
-  private String title;
+    @NotBlank(message = "Title is required")
+    private String title;
 
-  @NotBlank(message = "Author is required")
-  private String author;
+    @NotBlank(message = "Author is required")
+    private String author;
 
-  @NotBlank(message = "ISBN is required")
-  private String isbn;
+    @NotBlank(message = "ISBN is required")
+    private String isbn;
 
-  @NotBlank(message = "Content is required")
-  private String content;
+    @NotBlank(message = "Content is required")
+    private String content;
 
-  @NotNull(message = "Fixed price is required")
-  private int fixedPrice;
+    @NotNull(message = "Fixed price is required")
+    private int fixedPrice;
 
-  @NotNull(message = "Publication year is required")
-  private LocalDate publicationYear;
+    @NotNull(message = "Publication year is required")
+    private LocalDate publicationYear;
 
-  @NotNull(message = "Status is required")
-  private Product.Status status;
+    @NotNull(message = "Status is required")
+    private Product.Status status;
 
-  private int stockQuantity;
+    private int stockQuantity;
 
-  private String pictureUrl;
+    @NotNull(message = "Category IDs are required")
+    private List<Long> categoryIds;
 
-  @NotNull(message = "Category IDs are required")
-  private List<Long> categoryIds;
-
-  public Product toEntity() {
-    return Product.builder()
-        .title(title)
-        .author(author)
-        .isbn(isbn)
-        .content(content)
-        .fixedPrice(fixedPrice)
-        .publicationYear(publicationYear)
-        .status(Product.Status.AVAILABLE)
-        .build();
-  }
+    public Product toEntity() {
+        return Product.builder()
+            .title(title)
+            .author(author)
+            .isbn(isbn)
+            .content(content)
+            .fixedPrice(fixedPrice)
+            .publicationYear(publicationYear)
+            .status(Product.Status.AVAILABLE)
+            .build();
+    }
 }
