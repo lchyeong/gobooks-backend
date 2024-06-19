86bd7b563d49b614bc5783589c2a3cab3b57cb1a
[Cloud3] 이찬형 <tolnhappy5@naver.com>
2024-06-19 20:34:12 +0900

    fix: product 재고관련 수정

--- src/main/java/org/team/bookshop/domain/product/dto/ProductResponseDto.java
+++ src/main/java/org/team/bookshop/domain/product/dto/ProductResponseDto.java
@@ -13,46 +13,47 @@ import org.team.bookshop.domain.product.entity.Product.Status;
 @Getter
 public class ProductResponseDto {
 
-  private final Long id;
-  private final String title;
-  private final String author;
-  private final String isbn;
-  private final String content;
-  private final int fixedPrice;
-  private final LocalDate publicationYear;
-  private final Status status;
-  private final String pictureUrl;
-  private final LocalDateTime createdAt;
-  private final boolean discount;
-  private final int stockQuantity = 0;
+    private final Long id;
+    private final String title;
+    private final String author;
+    private final String isbn;
+    private final String content;
+    private final int fixedPrice;
+    private final LocalDate publicationYear;
+    private final Status status;
+    private final String pictureUrl;
+    private final LocalDateTime createdAt;
+    private final boolean discount;
+    private final int stockQuantity;
 
-  @JsonIgnoreProperties("bookCategories")
-  private List<SimpleCategoryResponseDto> categories;
+    @JsonIgnoreProperties("bookCategories")
+    private List<SimpleCategoryResponseDto> categories;
 
-  public ProductResponseDto(Product product) {
-    this.id = product.getId();
-    this.title = product.getTitle();
-    this.author = product.getAuthor();
-    this.isbn = product.getIsbn();
-    this.content = product.getContent();
-    this.fixedPrice = product.getFixedPrice();
-    this.publicationYear = product.getPublicationYear();
-    this.status = product.getStatus();
-    this.pictureUrl = product.getPictureUrl();
-    this.createdAt = product.getCreatedAt();
-    this.discount = product.isDiscount();
-    this.categories = product.getBookCategories().stream()
-        .map(bookCategory -> new SimpleCategoryResponseDto(bookCategory.getCategory()))
-        .collect(Collectors.toList());
-  }
+    public ProductResponseDto(Product product) {
+        this.id = product.getId();
+        this.title = product.getTitle();
+        this.author = product.getAuthor();
+        this.isbn = product.getIsbn();
+        this.content = product.getContent();
+        this.fixedPrice = product.getFixedPrice();
+        this.publicationYear = product.getPublicationYear();
+        this.status = product.getStatus();
+        this.pictureUrl = product.getPictureUrl();
+        this.createdAt = product.getCreatedAt();
+        this.discount = product.isDiscount();
+        this.stockQuantity = product.getStockQuantity();
+        this.categories = product.getBookCategories().stream()
+            .map(bookCategory -> new SimpleCategoryResponseDto(bookCategory.getCategory()))
+            .collect(Collectors.toList());
+    }
 
-  public static ProductResponseDto fromEntity(Product product) {
-    ProductResponseDto dto = new ProductResponseDto(product);
+    public static ProductResponseDto fromEntity(Product product) {
+        ProductResponseDto dto = new ProductResponseDto(product);
 
-    dto.categories = product.getBookCategories().stream()
-        .map(bookCategory -> new SimpleCategoryResponseDto(bookCategory.getCategory()))
-        .collect(Collectors.toList());
+        dto.categories = product.getBookCategories().stream()
+            .map(bookCategory -> new SimpleCategoryResponseDto(bookCategory.getCategory()))
+            .collect(Collectors.toList());
 
-    return dto;
-  }
+        return dto;
+    }
 }
